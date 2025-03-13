873d5e37f262af09017c9b065fa1d4e729fde587
hafidzmusyafa <hafidzmusyafa@student.telkomuniversity.ac.id>
2025-03-13 10:20:21 +0700

    Menambahkan class KodeProduk

--- Program.cs
+++ Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+public class KodeProduk
+{
+    public string Kode { get; set; }
+    public string Produk { get; set; }
+}
+
+class main
+{
+    public static void Main()
+    {
+        List<KodeProduk> isiKodeProduk = new List<KodeProduk>()
+        {
+            new KodeProduk { Produk = "Laptop", Kode = "E100" },
+            new KodeProduk { Produk = "SmartPhone", Kode = "E101" },
+            new KodeProduk { Produk = "Tablet", Kode = "E102" },
+            new KodeProduk { Produk = "Headset", Kode = "E103" },
+            new KodeProduk { Produk = "Keyboard", Kode = "E104" },
+            new KodeProduk { Produk = "Mouse", Kode = "E105" },
+            new KodeProduk { Produk = "Printer", Kode = "E106" },
+            new KodeProduk { Produk = "Monitor", Kode = "E107" },
+            new KodeProduk { Produk = "Smartwatch", Kode = "E108" },
+            new KodeProduk { Produk = "Kamera", Kode = "E109" }
+        };
+    }
+}
